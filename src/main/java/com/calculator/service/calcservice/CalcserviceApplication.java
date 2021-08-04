9f1d2af7b75b6420348ecf67f611d92bfab9dfc3
Debapriya <debapriya9b@gmail.com>
2021-08-04 22:32:19 +0100

    Initial commit_project calculator

---
+++ src/main/java/com/calculator/service/calcservice/CalcserviceApplication.java
@@ -0,0 +1,14 @@
+package com.calculator.service.calcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcserviceApplication {
+
+	public static void main(String[] args) {
+        //service container which will execute spring boot application
+		SpringApplication.run(CalcserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/calculator/service/calcservice/CalculatorService.java
@@ -0,0 +1,16 @@
+package com.calculator.service.calcservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorService {
+
+   @GetMapping("/add/{x}/{y}")
+    public Response add(@PathVariable int x, @PathVariable int y){
+       return new Response(x,y, x+y);
+   }
+
+
+}
